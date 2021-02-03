1a25d26c1effa6cc1787c5c0bf6e6d0d4b7ca909
Devon Wolfkiel <devonwolf@tutanota.com>
2021-02-02 17:07:35 -0800

    Fix merge conflicts

--- puzzles/puzzle-render-utils.js
+++ puzzles/puzzle-render-utils.js
@@ -30,33 +30,6 @@ const spaces = [
     pos9
 ];
 export function generateThreeByThree() {
-<<<<<<< HEAD
-    const tileMap = document.getElementById('tile-map');
-    tileMap.classList.add('tile-map');
-	
-    const pos1 = document.createElement('div');
-    const pos2 = document.createElement('div');
-    const pos3 = document.createElement('div');
-    const pos4 = document.createElement('div');
-    const pos5 = document.createElement('div');
-    const pos6 = document.createElement('div');
-    const pos7 = document.createElement('div');
-    const pos8 = document.createElement('div');
-    const pos9 = document.createElement('div');
-
-    const spaces = [
-        pos1,
-        pos2,
-        pos3,
-        pos4,
-        pos5,
-        pos6,
-        pos7,
-        pos8,
-        pos9
-    ];
-=======
->>>>>>> ca3f62fb7234961cb10844ab9e29b3f26ab73fa4
 
     const tiles = generateEightTiles();
 
@@ -87,12 +60,8 @@ export function generateEightTiles() {
     const tile6 = document.createElement('div');
     const tile7 = document.createElement('div');
     const tile8 = document.createElement('div');
-<<<<<<< HEAD
-	
-=======
     const tile9 = document.createElement('div');
     
->>>>>>> ca3f62fb7234961cb10844ab9e29b3f26ab73fa4
     const tiles = [
         tile1,
         tile2,
