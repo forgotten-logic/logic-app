df2110d22f82fbd9381e02a2c7b8f50eab754c5c
Vance-M <myveryvills@yahoo.com>
2021-02-03 16:29:15 -0800

    rollback shuffle functions to a working state

--- puzzles/puzzle.js
+++ puzzles/puzzle.js
@@ -1,3 +1,17 @@
-import { generateThreeByThree } from './puzzle-render-utils.js';
+import { generateThreeByThree, placeTilesRandomly } from './puzzle-render-utils.js';
+import { clearUserMoves } from './puzzle-utils.js';
+
+
+const resultsDisplay = document.getElementById('results-display');
+const startButton = document.createElement('button');
+resultsDisplay.parentNode.insertBefore(startButton, resultsDisplay);
+
+startButton.textContent = 'Shuffle tiles and start?';
+
+startButton.addEventListener('click', () => {
+    clearUserMoves();
+    placeTilesRandomly();
+    startButton.textContent = 'Shuffle tiles and start again?';
+});
 
 generateThreeByThree();
