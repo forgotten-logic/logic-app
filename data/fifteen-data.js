88136eb83c236f15d1865e88fc7550db36c5df56
Julianne Vela <cjvela@outlook.com>
2021-02-04 12:43:51 -0800

    added 15 puzzle info/data

---
+++ data/fifteen-data.js
@@ -0,0 +1,65 @@
+const fifteenData = [
+    {
+        id: 1,
+        position: 1,
+    },
+    {
+        id: 2,
+        position: 2,
+    },
+    {
+        id: 3,
+        position: 3,
+    },
+    {
+        id: 4,
+        position: 4,
+    },
+    {
+        id: 5,
+        position: 5,
+    },
+    {
+        id: 6,
+        position: 6,
+    },
+    {
+        id: 7,
+        position: 7,
+    },
+    {
+        id: 8,
+        position: 8,
+    },
+    {
+        id: 9,
+        position: 9,
+    },
+    {
+        id: 10,
+        position: 10,
+    },
+    {
+        id: 11,
+        position: 11,
+    },
+    {
+        id: 12,
+        position: 12,
+    },
+    {
+        id: 13,
+        position: 13,
+    },
+    {
+        id: 14,
+        position: 14,
+    },
+    {
+        id: 15,
+        isEmpty: true,
+        position: 15,
+    }
+];
+
+export default fifteenData;
--- data/puzzle-info.js
+++ data/puzzle-info.js
@@ -6,8 +6,13 @@ export const wikiLink = document.createElement('a');
 wikiLink.title = ' 15 Puzzle';
 
 export const eightPuzzle = {
-    id: 1,
+    id: 'eight-puzzle',
     name: '8 Puzzle',
-    description: `The 8 puzzle is a smaller variant of the popular <a href='https://en.wikipedia.org/wiki/15_puzzle' target='_blank'>${wikiLink.title}</a>. The goal is to slide the tiles around the board to line them up
-    sequentially from 1-8, with the empty space in the lower-left corner. `,
+    description: `The 8 puzzle is a smaller variant of the popular <a href='https://en.wikipedia.org/wiki/15_puzzle' target='_blank'>${wikiLink.title}</a>. The goal is to slide the tiles using the empty space to align them in sequential order with the empty space in the lower-right corner.`,
 };
+
+export const fifteenPuzzle = {
+    id: 'fifteen-puzzle',
+    name: '15 Puzzle',
+    description: 'The 15 puzzle is a sliding puzzle that consists of 14 numbered tiles and 1 blank space. The goal is to slide the tiles using the empty space to align them in sequential order with the empty space in the lower-right corner.'
+}
