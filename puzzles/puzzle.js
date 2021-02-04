80d73bc06240166931a167964bcbc8d5fc0cd4ef
Devon Wolfkiel <devonwolf@tutanota.com>
2021-02-04 10:02:36 -0800

    Disable tile movement until shuffle/start button is clicked

--- puzzles/puzzle.js
+++ puzzles/puzzle.js
@@ -1,18 +1,15 @@
-import { generateThreeByThree, placeTilesRandomly, generatePuzzleInfo } from './puzzle-render-utils.js';
+import { generateThreeByThree, placeTilesRandomly, generatePuzzleInfo, startGame } from './puzzle-render-utils.js';
 import { clearUserMoves } from './puzzle-utils.js';
 
 
 const resultsDisplay = document.getElementById('results-display');
 const startButton = document.createElement('button');
 resultsDisplay.parentNode.insertBefore(startButton, resultsDisplay);
-
 startButton.textContent = 'Shuffle tiles and start?';
 
-startButton.addEventListener('click', () => {
-    clearUserMoves();
-    placeTilesRandomly();
-    startButton.textContent = 'Shuffle tiles and start again?';
-});
+startButton.addEventListener('click', startGame);
 
 generatePuzzleInfo();
 generateThreeByThree();
+
+
